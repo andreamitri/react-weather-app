7c0e0af07d8596c67cda03ef67cbefcd1e721b9a
andreamitri <ladanandrea@gmail.com>
2024-08-22 22:40:29 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Andrea Mitri and is{" "}
-        <a
-          href="https://github.com/andreamitri/react-weather-app"
-          target="blank"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Andrea Mitri and is{" "}
+          <a
+            href="https://github.com/andreamitri/react-weather-app"
+            target="blank"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
