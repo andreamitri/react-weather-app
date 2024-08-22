032e2a5d6c98e186b5a589a4984e74e7d74ca6ba
andreamitri <ladanandrea@gmail.com>
2024-08-22 22:03:54 +0200

    Added the github repository url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather app</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by Andrea Mitri and is{" "}
+        <a
+          href="https://github.com/andreamitri/react-weather-app"
+          target="blank"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
