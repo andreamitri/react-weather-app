09bb481b92fa339481d9eb25438887ffe3238a41
andreamitri <ladanandrea@gmail.com>
2024-10-17 22:05:34 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+
 import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 import axios from "axios";
